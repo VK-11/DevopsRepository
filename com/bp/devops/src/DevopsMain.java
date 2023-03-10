08e4687c28e6d82088021f0fee98172c08acce47
Vikas Kaushik <vk@Vikass-MacBook-Pro.local>
2023-03-10 22:07:08 +0530

    devops assignment initial commit

---
+++ com/bp/devops/src/DevopsMain.java
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- saved from url=(0066)https://introcs.cs.princeton.edu/java/11hello/HelloWorld.java.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="shortcut icon" href="https://introcs.cs.princeton.edu/favicon.ico">
+<link rel="stylesheet" href="./HelloWorld.java_files/java.css" type="text/css">
+
+<title>HelloWorld.java</title>
+
+
+<meta name="AUTHOR" content="Robert Sedgewick and Kevin Wayne">
+<meta name="DESCRIPTION" content="HelloWorld code in Java">
+<meta name="TITLE" content="HelloWorld code in Java">
+<meta name="KEYWORDS" content="HelloWorld,java,programming,computer science,algorithm,program,code">
+<meta name="ROBOTS" content="INDEX,FOLLOW">
+
+</head>
+
+
+<body data-new-gr-c-s-check-loaded="14.1100.0" data-gr-ext-installed="">
+<center><h1>HelloWorld.java</h1></center><p><br>
+
+Below is the syntax highlighted version of <a href="https://introcs.cs.princeton.edu/java/11hello/HelloWorld.java">HelloWorld.java</a>
+from <a href="https://introcs.cs.princeton.edu/java/11hello">§1.1 Hello World</a>.
+</p><p><br>
+
+<!-- Generator: GNU source-highlight 3.1.8
+by Lorenzo Bettini
+http://www.lorenzobettini.it
+http://www.gnu.org/software/src-highlite -->
+</p><pre><tt><span class="comment">/******************************************************************************</span>
+<span class="comment"> *  Compilation:  javac HelloWorld.java</span>
+<span class="comment"> *  Execution:    java HelloWorld</span>
+<span class="comment"> *</span>
+<span class="comment"> *  Prints "Hello, World". By tradition, this is everyone's first program.</span>
+<span class="comment"> *</span>
+<span class="comment"> *  % java HelloWorld</span>
+<span class="comment"> *  Hello, World</span>
+<span class="comment"> *</span>
+<span class="comment"> *  These 17 lines of text are comments. They are not part of the program;</span>
+<span class="comment"> *  they serve to remind us about its properties. The first two lines tell</span>
+<span class="comment"> *  us what to type to compile and test the program. The next line describes</span>
+<span class="comment"> *  the purpose of the program. The next few lines give a sample execution</span>
+<span class="comment"> *  of the program and the resulting output. We will always include such</span>
+<span class="comment"> *  lines in our programs and encourage you to do the same.</span>
+<span class="comment"> *</span>
+<span class="comment"> ******************************************************************************/</span>
+
+<span class="keyword">public</span><span class="normal"> </span><span class="keyword">class</span><span class="normal"> </span><span class="classname">HelloWorld</span><span class="normal"> </span><span class="cbracket">{</span>
+
+<span class="normal">    </span><span class="keyword">public</span><span class="normal"> </span><span class="keyword">static</span><span class="normal"> </span><span class="type">void</span><span class="normal"> </span><span class="function">main</span><span class="symbol">(</span><span class="normal">String</span><span class="symbol">[]</span><span class="normal"> args</span><span class="symbol">)</span><span class="normal"> </span><span class="cbracket">{</span>
+
+<span class="normal">        </span><span class="comment">// Prints "Hello, World" in the terminal window.</span>
+<span class="normal">        System</span><span class="symbol">.</span><span class="normal">out</span><span class="symbol">.</span><span class="function">println</span><span class="symbol">(</span><span class="string">"Hello, World"</span><span class="symbol">);</span>
+<span class="normal">    </span><span class="cbracket">}</span>
+
+<span class="cbracket">}</span>
+</tt></pre>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script src="./HelloWorld.java_files/ga.js" type="text/javascript"></script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-10811519-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+
+
+<p><br></p><address><small>
+Copyright © 2000–2022, Robert Sedgewick and Kevin Wayne.
+<br>Last updated: Thu Aug 11 10:11:56 EDT 2022.
+</small></address>
+
+
+</body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
